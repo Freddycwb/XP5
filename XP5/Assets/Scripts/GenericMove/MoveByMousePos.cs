70cf303b6f76575c869c69d58635abf73a12dd6f
Freddy Matheus <45660002+Freddycwb@users.noreply.github.com>
2024-05-17 00:18:19 -0300

    Fix Window Move

--- XP5/Assets/Scripts/GenericMove/MoveByMousePos.cs
+++ XP5/Assets/Scripts/GenericMove/MoveByMousePos.cs
@@ -9,15 +9,21 @@ public class MoveByMousePos : MonoBehaviour
     private Vector3 mouse;
     private RectTransform rectTransform;
 
+    [SerializeField] private GameObjectVariable canvasObj;
+    private Canvas canvas;
+
     private void OnEnable()
     {
         rectTransform = GetComponent<RectTransform>();
+        canvas = canvasObj.Value.GetComponent<Canvas>();
+        startPos = rectTransform.localPosition;
         mouse = Input.mousePosition;
-        startPos = rectTransform.position;
     }
 
     void Update()
     {
-        rectTransform.position = startPos + (Input.mousePosition - mouse);
+        Vector3 canvasLocalScale = canvas.transform.localScale;
+        Vector3 mouseDiff = (Input.mousePosition - mouse) / canvas.scaleFactor;
+        rectTransform.localPosition = startPos + mouseDiff;
     }
 }
